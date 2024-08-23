b85606dc69fb7deca719bc53ac38a37901822013
Mayara Aiko <mayara.aiko@yahoo.com.br>
2024-08-23 09:57:05 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, status, Depends
+import classes
+import model
+from database import engine, get_db
+from sqlalchemy.orm import Session
+
+model.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+ return {"Hello": "lala"}
+
+@app.post("/criar", status_code=status.HTTP_201_CREATED)
+def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
+ mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
+ db.add(mensagem_criada)
+ db.commit()
+ db.refresh(mensagem_criada)
+ return {"Mensagem": mensagem_criada}
+
+@app.get("/quadrado/{num}")
+def square(num: int):
+    return num ** 2
