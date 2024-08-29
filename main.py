efa101dbd81687613f38c9bd1216ddd328db32d9
Mayara Aiko <mayara.aiko@yahoo.com.br>
2024-08-29 09:34:03 -0300

    testing yml

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status, Depends
 import classes
 import model
+from model import Base 
 from database import engine, get_db
 from sqlalchemy.orm import Session
 
@@ -13,11 +14,11 @@ def read_root():
 
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
- mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
- db.add(mensagem_criada)
- db.commit()
- db.refresh(mensagem_criada)
- return {"Mensagem": mensagem_criada}
+    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
+    db.add(mensagem_criada)
+    db.commit()
+    db.refresh(mensagem_criada)
+    return {"Mensagem": mensagem_criada}
 
 @app.get("/quadrado/{num}")
 def square(num: int):
